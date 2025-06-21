052dc257e2a4c75c46e14234a791b3717085a7a3
Susumu Yagi <susumu.yagi@gmail.com>
2025-06-21 09:44:14 +0900

    state and json

--- playground/syagi/websocket/server.js
+++ playground/syagi/websocket/server.js
@@ -3,16 +3,24 @@ const WebSocket = require('ws');
 // WebSocketサーバーを起動（ポート8080）
 const wss = new WebSocket.Server({ port: 8080 });
 
+let state = {
+  count: 0,
+};
+
 wss.on('connection', (ws) => {
   console.log('新しいクライアントが接続しました');
 
   // クライアントからのメッセージ受信
   ws.on('message', (message) => {
     console.log(`受信: ${message}`);
+    state.count += 1;
     // 全クライアントにメッセージをブロードキャスト
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify({
+              message: String(message),
+              count: state.count
+            }));
       }
     });
   });
