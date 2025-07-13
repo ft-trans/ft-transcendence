303a92dac297b38823a4936802f99f3e1d297875
tkuramot <mabdman512@gmail.com>
2025-07-13 16:11:24 +0900

    add description for triple slash directive

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// the following line is needed to extend the types for vitest
 /// <reference types="vitest/config" />
 import viteFastify from "@fastify/vite/plugin";
 import tailwindcss from "@tailwindcss/vite";
