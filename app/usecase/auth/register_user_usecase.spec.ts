1659f1aa5d4812db353718e59f459908b8ec9064
tkuramot <mabdman512@gmail.com>
2025-08-05 01:37:33 +0900

    replace mocks with vitest-mock-extended

--- app/usecase/auth/register_user_usecase.spec.ts
+++ app/usecase/auth/register_user_usecase.spec.ts
@@ -3,22 +3,8 @@ import { User, UserEmail } from "@domain/model";
 import type { IUserRepository } from "@domain/repository";
 import type { ITransaction } from "@usecase/transaction";
 import { beforeEach, describe, expect, it, vi } from "vitest";
+import { mock } from "vitest-mock-extended";
 import { RegisterUserUsecase } from "./register_user_usecase";
-
-const mockUserRepository: IUserRepository = {
-	create: vi.fn(),
-	findById: vi.fn(),
-	findByEmail: vi.fn(),
-};
-
-const tx: ITransaction = {
-	exec: vi.fn().mockImplementation(async (callback) => {
-		const repo = {
-			newUserRepository: () => mockUserRepository,
-		};
-		return callback(repo);
-	}),
-};
 
 describe("RegisterUserUsecase", () => {
 	beforeEach(() => {
@@ -27,22 +13,37 @@ describe("RegisterUserUsecase", () => {
 
 	it("should create a new user and return it", async () => {
 		const expectedUser = User.create(new UserEmail("test@example.com"));
-		vi.mocked(mockUserRepository.create).mockResolvedValue(expectedUser);
-		vi.mocked(mockUserRepository.findByEmail).mockResolvedValue(undefined);
+		const mockUserRepo = mock<IUserRepository>();
+		mockUserRepo.create.mockResolvedValue(expectedUser);
+		mockUserRepo.findByEmail.mockResolvedValue(undefined);
+		const mockTx = mock<ITransaction>();
+		mockTx.exec.mockImplementation(async (callback) => {
+			const repo = {
+				newUserRepository: () => mockUserRepo,
+			};
+			return callback(repo);
+		});
 
-		const usecase = new RegisterUserUsecase(tx);
+		const usecase = new RegisterUserUsecase(mockTx);
 		const input = { email: "test@example.com" };
 		const user = await usecase.execute(input);
 
 		expect(user.email).toEqual(expectedUser.email);
-		expect(tx.exec).toHaveBeenCalledOnce();
 	});
 
 	it("should throw BadRequestError if email is already used", async () => {
 		const existingUser = User.create(new UserEmail("test@example.com"));
-		vi.mocked(mockUserRepository.findByEmail).mockResolvedValue(existingUser);
+		const mockUserRepo = mock<IUserRepository>();
+		mockUserRepo.findByEmail.mockResolvedValue(existingUser);
+		const mockTx = mock<ITransaction>();
+		mockTx.exec.mockImplementation(async (callback) => {
+			const repo = {
+				newUserRepository: () => mockUserRepo,
+			};
+			return callback(repo);
+		});
 
-		const usecase = new RegisterUserUsecase(tx);
+		const usecase = new RegisterUserUsecase(mockTx);
 		const input = { email: "test@example.com" };
 
 		await expect(usecase.execute(input)).rejects.toThrow(BadRequestError);
