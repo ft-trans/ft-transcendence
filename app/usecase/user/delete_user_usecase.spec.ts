55003678cb8e06af27029d81947fd111eb2e3ef4
Susumu Yagi <susumu.yagi@gmail.com>
2025-08-07 14:58:05 +0900

    implement delete user usecase

---
+++ app/usecase/user/delete_user_usecase.spec.ts
@@ -0,0 +1,57 @@
+import { NotFoundError } from "@domain/error";
+import { User, UserEmail } from "@domain/model";
+import type { IUserRepository } from "@domain/repository";
+import type { ITransaction } from "@usecase/transaction";
+import { ulid } from "ulid";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { mock } from "vitest-mock-extended";
+import { DeleteUserUsecase } from "./delete_user_usecase";
+
+describe("DeleteUserUsecase", () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it("should delete a user and return it", async () => {
+		const currentUser = User.create(new UserEmail("current@example.com"));
+		const mockUserRepo = mock<IUserRepository>();
+		mockUserRepo.delete.mockResolvedValue(currentUser);
+		mockUserRepo.findById.mockResolvedValue(currentUser);
+
+		const mockTx = mock<ITransaction>();
+		mockTx.exec.mockImplementation(async (callback) => {
+			const repo = {
+				newUserRepository: () => mockUserRepo,
+			};
+			return callback(repo);
+		});
+
+		const usecase = new DeleteUserUsecase(mockTx);
+		const input = { id: currentUser.id.value };
+		const user = await usecase.execute(input);
+
+		expect(user.email).toEqual(currentUser.email);
+		expect(mockTx.exec).toHaveBeenCalledOnce();
+	});
+
+	it("should throw NotFoundError if user does not exist", async () => {
+		const mockUserRepo = mock<IUserRepository>();
+		mockUserRepo.delete.mockResolvedValue(undefined);
+		mockUserRepo.findById.mockResolvedValue(undefined);
+
+		const mockTx = mock<ITransaction>();
+		mockTx.exec.mockImplementation(async (callback) => {
+			const repo = {
+				newUserRepository: () => mockUserRepo,
+			};
+			return callback(repo);
+		});
+
+		const usecase = new DeleteUserUsecase(mockTx);
+		const input = { id: ulid() };
+
+		await expect(usecase.execute(input)).rejects.toThrow(NotFoundError);
+		expect(mockUserRepo.delete).not.toHaveBeenCalled();
+		expect(mockTx.exec).toHaveBeenCalledOnce();
+	});
+});
---
+++ app/usecase/user/delete_user_usecase.ts
@@ -0,0 +1,25 @@
+import { NotFoundError } from "@domain/error";
+import { type User, UserId } from "@domain/model";
+import type { ITransaction } from "@usecase/transaction";
+
+export type DeleteUserUsecaseInput = {
+	id: string;
+};
+
+export class DeleteUserUsecase {
+	constructor(private readonly tx: ITransaction) {}
+
+	async execute(input: DeleteUserUsecaseInput): Promise<User> {
+		const user = await this.tx.exec(async (repo) => {
+			const userRepo = repo.newUserRepository();
+			const userId = new UserId(input.id);
+			const currentUser = await userRepo.findById(userId);
+			if (!currentUser) {
+				throw new NotFoundError();
+			}
+
+			return await userRepo.delete(userId);
+		});
+		return user;
+	}
+}
