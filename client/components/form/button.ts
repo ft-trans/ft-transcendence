c5a64dbdd9cba54d60f2aad87c4767f3070e2cc5
Susumu Yagi <susumu.yagi@gmail.com>
2025-08-24 10:31:42 +0900

    implement delete user front

--- client/components/form/button.ts
+++ client/components/form/button.ts
@@ -1,6 +1,6 @@
 import { Component } from "../component";
 
-type ButtonColor = "blue" | "gray";
+type ButtonColor = "blue" | "red" | "gray";
 type ButtonWidth = "full";
 type ButtonType = "submit" | "button";
 
@@ -9,6 +9,7 @@ type Props = {
 	width?: ButtonWidth;
 	type?: ButtonType;
 	text: string;
+	id?: string;
 };
 
 export class Button extends Component {
@@ -16,27 +17,41 @@ export class Button extends Component {
 	private readonly width: ButtonWidth | undefined;
 	private readonly type: ButtonType;
 	private readonly text: string;
+	private readonly id: string | undefined;
 
-	constructor({ color = "blue", width, type = "button", text }: Props) {
+	constructor({ color = "blue", width, type = "button", text, id }: Props) {
 		super();
 		this.color = color;
 		this.width = width;
 		this.type = type;
 		this.text = text;
+		this.id = id;
 	}
 
 	render(): string {
 		const widthClass = this.width === "full" ? "w-full" : "";
 
-		if (this.color === "gray") {
-			return `
-        <button type="${this.type}" class="bg-gray-200 text-gray-800 px-6 py-3 rounded-lg hover:bg-gray-300 transition-colors ${widthClass}">
-          ${this.text}
-        </button>
-      `;
+		const styleClass = [
+			"px-6",
+			"py-3",
+			"rounded-lg",
+			"transition-colors",
+			"cursor-pointer",
+			widthClass,
+		];
+
+		if (this.color === "blue") {
+			styleClass.push("bg-blue-600", "hover:bg-blue-700", "text-white");
+		} else if (this.color === "red") {
+			styleClass.push("bg-red-600", "hover:bg-red-700", "text-white");
+		} else if (this.color === "gray") {
+			styleClass.push("bg-gray-200", "hover:bg-gray-300", "text-gray-800");
 		}
+
+		const idAttribute = this.id ? `id="${this.id}"` : "";
+
 		return `
-      <button type="${this.type}" class="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors ${widthClass}">
+      <button ${idAttribute} type="${this.type}" class="${styleClass.join(" ")}">
         ${this.text}
       </button>
     `;
