432c375d7200893e4c3fd1b06a8907d7e075ffce
Susumu Yagi <susumu.yagi@gmail.com>
2025-08-27 15:07:04 +0900

    add pong usecase

---
+++ app/usecase/pong/start_pong_usecase.ts
@@ -0,0 +1,25 @@
+import { Ball, MatchId } from "@domain/model/pong";
+import type { IKVSRepository } from "@domain/repository";
+import { PongField } from "@shared/api/pong";
+
+export type StartPongUsecaseInput = {
+	matchId: string;
+};
+
+export class StartPongUsecase {
+	constructor(private readonly repo: IKVSRepository) {}
+
+	async execute(input: StartPongUsecaseInput): Promise<MatchId> {
+		const matchId = new MatchId(input.matchId);
+
+		const x = PongField.width / 2;
+		const y = PongField.height * Math.random();
+		const vx = 5 * Math.random() + 1;
+		const vy = 5 * Math.random() + 1;
+		const ball = new Ball(x, y, vx, vy);
+
+		await this.repo.newBallRepository().set(matchId, ball);
+
+		return matchId;
+	}
+}
--- shared/api/pong.ts
+++ shared/api/pong.ts
@@ -1,10 +1,18 @@
 export type PongGameEvent = "gameState";
 
-export type PongGameState = {
+export type PongGameStatePayload = {
+	ball: { x: number; y: number; vx: number; vy: number };
+	//   paddles: { player1: number; player2: number };
+	//   score: { player1: number; player2: number };
+};
+
+export type PongGameStateResponse = {
 	event: PongGameEvent;
-	payload: {
-		ball: { x: number; y: number; vx: number; vy: number };
-		//   paddles: { player1_y: number; player2_y: number };
-		//   score: { player1: number; player2: number };
-	};
+	payload: PongGameStatePayload;
+	matchid: string;
+};
+
+export const PongField = {
+	width: 600,
+	height: 400,
 };
