98fb7aa1f13110e40e5dcc5d7579f34bf3c8a2e2
Susumu Yagi <susumu.yagi@gmail.com>
2025-08-28 14:50:49 +0900

    fix connectedClients map key (MatchId -> string)

--- app/infra/kvs/ball_repository.ts
+++ app/infra/kvs/ball_repository.ts
@@ -6,20 +6,20 @@ export class BallRepository implements IBallRepository {
 	constructor(private readonly client: Client) {}
 
 	async set(matchId: MatchId, ball: Ball): Promise<Ball> {
-		this.client.set(this.ballKey(matchId), JSON.stringify(ball));
+		this.client.set(this.getKey(matchId), JSON.stringify(ball));
 		return ball;
 	}
 
 	async get(matchId: MatchId): Promise<Ball | undefined> {
-		const data = await this.client.get(this.ballKey(matchId));
+		const data = await this.client.get(this.getKey(matchId));
 		return data ? JSON.parse(data) : undefined;
 	}
 
 	async delete(matchId: MatchId): Promise<void> {
-		this.client.del(this.ballKey(matchId));
+		this.client.del(this.getKey(matchId));
 	}
 
-	private ballKey(matchId: MatchId): string {
+	private getKey(matchId: MatchId): string {
 		return `match:${matchId.value}:ball`;
 	}
 }
