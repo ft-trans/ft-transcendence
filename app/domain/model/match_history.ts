f266114b7a3f3cebbd481cab619ecbd33e89848a
nktr-cp <nktrcp@gmail.com>
2025-08-29 14:23:42 +0900

    [feat] deal users with their ids

--- app/domain/model/match_history.ts
+++ app/domain/model/match_history.ts
@@ -1,11 +1,22 @@
 import { ulid } from "ulid";
-import type { User } from "./user";
+import type { User, UserEmail, UserId } from "./user";
+import { ValueObject } from "./value_object";
+
+export class MatchHistoryId extends ValueObject<string, "MatchHistoryId"> {
+	protected validate(value: string): void {
+		if (value.length === 0) {
+			throw new Error("MatchHistoryId cannot be empty");
+		}
+	}
+}
 
 export class MatchHistory {
 	constructor(
-		readonly id: string,
-		readonly winner: User,
-		readonly loser: User,
+		readonly id: MatchHistoryId,
+		readonly winnerId: UserId,
+		readonly loserId: UserId,
+		readonly winnerEmail: UserEmail,
+		readonly loserEmail: UserEmail,
 		readonly winnerScore: number,
 		readonly loserScore: number,
 		readonly playedAt: Date,
@@ -17,11 +28,13 @@ export class MatchHistory {
 		winnerScore: number,
 		loserScore: number,
 	): MatchHistory {
-		const id = ulid();
+		const id = new MatchHistoryId(ulid());
 		return new MatchHistory(
 			id,
-			winner,
-			loser,
+			winner.id,
+			loser.id,
+			winner.email,
+			loser.email,
 			winnerScore,
 			loserScore,
 			new Date(),
