71c9383e0171d930fe52bec92238013bc9188725
nktr-cp <nktrcp@gmail.com>
2025-08-29 14:24:54 +0900

    [feat] added functions in direct message repository

--- app/domain/repository/direct_message_repository.ts
+++ app/domain/repository/direct_message_repository.ts
@@ -2,6 +2,8 @@ import type { DirectMessage } from "../model/direct_message";
 
 export interface IDirectMessageRepository {
 	save(message: DirectMessage): Promise<DirectMessage>;
+	findById(messageId: string): Promise<DirectMessage | undefined>;
+	update(message: DirectMessage): Promise<DirectMessage>;
 	findMessagesBetweenUsers(
 		userId1: string,
 		userId2: string,
