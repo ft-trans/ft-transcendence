c149c17a39e314e7ba168f5c4965070642dad2d4
nktr-cp <nktrcp@gmail.com>
2025-08-29 14:25:51 +0900

    [fix] friendship repository null -> undefined

--- app/domain/repository/friendship_repository.ts
+++ app/domain/repository/friendship_repository.ts
@@ -3,7 +3,10 @@ import type { User } from "../model/user";
 
 export interface IFriendshipRepository {
 	save(friendship: Friendship): Promise<Friendship>;
-	findByUserIds(userId1: string, userId2: string): Promise<Friendship | null>;
+	findByUserIds(
+		userId1: string,
+		userId2: string,
+	): Promise<Friendship | undefined>;
 	findFriendsByUserId(userId: string): Promise<User[]>;
 	findPendingRequestsByReceiverId(userId: string): Promise<Friendship[]>;
 	delete(friendship: Friendship): Promise<void>;
