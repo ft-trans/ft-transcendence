f15fe16c4ee41b56f11d12d82143e5d1be7f5892
nktr-cp <nktrcp@gmail.com>
2025-08-29 14:26:35 +0900

    [fix] match making service, null -> undefined

--- app/domain/service/matchmaking_service.ts
+++ app/domain/service/matchmaking_service.ts
@@ -3,47 +3,50 @@ import { ErrBadRequest } from "../error";
 import { Match } from "../model/match";
 import type { User } from "../model/user";
 import type { IMatchRepository } from "../repository/match_repository";
-
-export interface IMatchmakingQueue {
-	add(user: User): Promise<void>;
-	remove(userId: string): Promise<void>;
-	findMatch(): Promise<[User, User] | null>;
-}
+import type { IMatchmakingQueueRepository } from "../repository/matchmaking_queue_repository";
 
 export class MatchmakingService {
 	constructor(
 		private readonly transaction: ITransaction,
 		private readonly matchRepository: IMatchRepository,
-		private readonly matchmakingQueue: IMatchmakingQueue,
+		private readonly matchmakingQueueRepository: IMatchmakingQueueRepository,
 	) {}
 
-	async join(user: User): Promise<Match | null> {
+	async join(user: User): Promise<Match | undefined> {
 		// 既に進行中の試合がないか確認
 		const existingMatch =
 			await this.matchRepository.findInProgressMatchByUserId(user.id.value);
 		if (existingMatch) {
-			throw new ErrBadRequest({ userMessage: "User is already in a match." });
+			throw new ErrBadRequest({
+				userMessage: "ユーザーは既に試合に参加しています。",
+			});
 		}
 
-		await this.matchmakingQueue.add(user);
-		const matchedUsers = await this.matchmakingQueue.findMatch();
+		await this.matchmakingQueueRepository.add(user);
+		const matchedUsers = await this.matchmakingQueueRepository.pop();
 
 		if (matchedUsers) {
-			const newMatch: Match | null = await this.transaction.exec(async () => {
-				const match = Match.create(matchedUsers);
-				const savedMatch = await this.matchRepository.save(match);
-				// キューからマッチしたユーザーを削除
-				await this.matchmakingQueue.remove(matchedUsers[0].id.value);
-				await this.matchmakingQueue.remove(matchedUsers[1].id.value);
-				return savedMatch;
-			});
+			const newMatch: Match | undefined = await this.transaction.exec(
+				async () => {
+					const match = Match.create(matchedUsers);
+					const savedMatch = await this.matchRepository.save(match);
+					// キューからマッチしたユーザーを削除
+					await this.matchmakingQueueRepository.remove(
+						matchedUsers[0].id.value,
+					);
+					await this.matchmakingQueueRepository.remove(
+						matchedUsers[1].id.value,
+					);
+					return savedMatch;
+				},
+			);
 			// WebSocketなどでマッチング成功を通知する
 			return newMatch;
 		}
-		return null;
+		return undefined;
 	}
 
 	async leave(userId: string): Promise<void> {
-		await this.matchmakingQueue.remove(userId);
+		await this.matchmakingQueueRepository.remove(userId);
 	}
 }
