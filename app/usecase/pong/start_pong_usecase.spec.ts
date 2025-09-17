14d4f2cd762d4efa759e659d1d6ecd21dd133288
Susumu Yagi <susumu.yagi@gmail.com>
2025-09-17 16:41:35 +0900

    add pong test

---
+++ app/usecase/pong/start_pong_usecase.spec.ts
@@ -0,0 +1,41 @@
+import type {
+	IDirectMessageRepository,
+	IFriendshipRepository,
+	IPongBallRepository,
+	IPongClientRepository,
+	IPongLoopRepository,
+	IUserRepository,
+} from "@domain/repository";
+import { ulid } from "ulid";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { mock } from "vitest-mock-extended";
+import { StartPongUsecase } from "./start_pong_usecase";
+
+const pongBallRepo = mock<IPongBallRepository>();
+
+const repo = {
+	newUserRepository: () => mock<IUserRepository>(),
+	newFriendshipRepository: () => mock<IFriendshipRepository>(),
+	newDirectMessageRepository: () => mock<IDirectMessageRepository>(),
+	newPongBallRepository: () => pongBallRepo,
+	newPongClientRepository: () => mock<IPongClientRepository>(),
+	newPongLoopRepository: () => mock<IPongLoopRepository>(),
+};
+
+describe("StartPongUsecase", () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it("should start a new pong match", async () => {
+		const matchId = ulid();
+
+		const usecase = new StartPongUsecase(repo);
+		const input = { matchId: matchId };
+		const ret = await usecase.execute(input);
+
+		expect(ret.value).toBe(matchId);
+
+		expect(repo.newPongBallRepository().set).toHaveBeenCalledOnce();
+	});
+});
